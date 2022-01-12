16399c47c3c0b28787b90ba85e35e8113ae17918
BrandonPSilva <brandon@bsilva.dev>
2022-01-12 18:48:47 -0500

    updcated print statements

--- league_rank_tracker.py
+++ league_rank_tracker.py
@@ -33,11 +33,12 @@ for i in range(len(y)):
     losses = (y[i]['losses'])
     val = (NAME, CURRENT_SEASON, tier, rank, leaguePoints, wins, losses)
 
-print(val)
-
 pdo = mydb.cursor()
 pdo.execute("SELECT name, season, tier, rank, leaguePoints, wins, losses FROM league_rank_tracker ORDER BY id DESC LIMIT 1")
 myresult = pdo.fetchall()
+
+print("Last database entry: " + str(myresult[0]))
+print("Current API pull: " + str(val))
 
 if (myresult[0] != val):
   sql = "INSERT INTO league_rank_tracker (name, season, tier, rank, leaguePoints, wins, losses) VALUES (%s, %s, %s, %s, %s, %s, %s)"
